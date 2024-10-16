e985e5244df76257e64939bdc2ff662decd1dbc4
wsj2681 <wsj2681@naver.com>
2024-10-16 15:51:42 +0900

    DirectX device initializing

---
+++ Windows-API/Main/framework.h
@@ -0,0 +1,130 @@
+﻿// header.h: 표준 시스템 포함 파일
+// 또는 프로젝트 특정 포함 파일이 들어 있는 포함 파일입니다.
+//
+
+#pragma once
+
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+// Windows 헤더 파일
+#include <windows.h>
+// C 런타임 헤더 파일입니다.
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+#define SCREEN_WIDTH  800
+#define SCREEN_HEIGHT 600
+
+#include <d3d11.h>
+#include <dxgi1_4.h>
+#include <D3Dcompiler.h>
+#include <DirectXPackedVector.h>
+#include <DirectXColors.h>
+#include <DirectXCollision.h>
+#include <DirectXMath.h>
+#include <Mmsystem.h>
+
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "dxguid.lib")
+
+IDXGISwapChain* swapchain;
+ID3D11Device* dev;
+ID3D11DeviceContext* devcon;
+ID3D11RenderTargetView* backbuffer;
+
+void InitD3D(HWND hWnd)
+{
+    // create a struct to hold information about the swap chain
+    DXGI_SWAP_CHAIN_DESC scd;
+
+    // clear out the struct for use
+    ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+    // fill the swap chain description struct
+    scd.BufferCount = 1;                                    // one back buffer
+    scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;     // use 32-bit color
+    scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;      // how swap chain is to be used
+    scd.OutputWindow = hWnd;                                // the window to be used
+    scd.SampleDesc.Count = 4;                               // how many multisamples
+    scd.Windowed = TRUE;                                    // windowed/full-screen mode
+
+    // create a device, device context and swap chain using the information in the scd struct
+    D3D11CreateDeviceAndSwapChain(NULL,
+        D3D_DRIVER_TYPE_HARDWARE,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        D3D11_SDK_VERSION,
+        &scd,
+        &swapchain,
+        &dev,
+        NULL,
+        &devcon);
+
+    /*
+    HRESULT D3D11CreateDeviceAndSwapChain(
+    IDXGIAdapter *pAdapter,
+    D3D_DRIVER_TYPE DriverType,
+    HMODULE Software,
+    UINT Flags,
+    D3D_FEATURE_LEVEL *pFeatureLevels,
+    UINT FeatureLevels,
+    UINT SDKVersion,
+    DXGI_SWAP_CHAIN_DESC *pSwapChainDesc,
+    IDXGISwapChain **ppSwapChain,
+    ID3D11Device **ppDevice,
+    D3D_FEATURE_LEVEL *pFeatureLevel,
+    ID3D11DeviceContext **ppDeviceContext);
+    */
+
+    // get the address of the back buffer
+    ID3D11Texture2D* pBackBuffer;
+    swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+
+    // use the back buffer address to create the render target
+    dev->CreateRenderTargetView(pBackBuffer, NULL, &backbuffer);
+    pBackBuffer->Release();
+
+    // set the render target as the back buffer
+    devcon->OMSetRenderTargets(1, &backbuffer, NULL);
+
+
+    // Set the viewport
+    D3D11_VIEWPORT viewport;
+    ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
+
+    viewport.TopLeftX = 0;
+    viewport.TopLeftY = 0;
+    viewport.Width = SCREEN_WIDTH;
+    viewport.Height = SCREEN_HEIGHT;
+
+    devcon->RSSetViewports(1, &viewport);
+
+}
+
+void CleanD3D()
+{
+    // close and release all existing COM objects
+    swapchain->Release();
+    backbuffer->Release();
+    dev->Release();
+    devcon->Release();
+}
+
+void RenderFrame(void)
+{
+    // clear the back buffer to a deep blue
+    float color[4] = { 0.0f, 0.2f, 0.4f, 1.0f };
+
+    devcon->ClearRenderTargetView(backbuffer, color);
+
+    // do 3D rendering on the back buffer here
+
+    // switch the back buffer and the front buffer
+    swapchain->Present(0, 0);
+}
